1d5bab2b3326de1d26ace955aae06217daf1bbb5
Deniz Kandemir <denizkandemir137@gmail.com>
2025-09-19 17:29:23 +0300

    styles updated

---
+++ monsters.js
@@ -0,0 +1,58 @@
+const monsters =  [
+  {
+    "id": "0-3-8",
+    "name": "Rapvolose",
+    "category": "Volant",
+    "form": "Raptix",
+    "color": { "name": "Rose", "hex": "#ec4899" },
+    "stats": { "hp": 7, "power": 3, "stars": 1 }
+  },
+  {
+    "id": "1-5-1",
+    "name": "Hersolzur",
+    "category": "Sol",
+    "form": "Herizo",
+    "color": { "name": "Azur", "hex": "#3b82f6" },
+    "stats": { "hp": 1, "power": 3, "stars": 0 }
+  },
+  {
+    "id": "2-2-5",
+    "name": "Staplabre",
+    "category": "Plafond",
+    "form": "Stalax",
+    "color": { "name": "Ombre", "hex": "#374151" },
+    "stats": { "hp": 7, "power": 1, "stars": 1 }
+  },
+  {
+    "id": "0-0-3",
+    "name": "Ailvolor",
+    "category": "Volant",
+    "form": "Aileron",
+    "color": { "name": "Or", "hex": "#f59e0b" },
+    "stats": { "hp": 3, "power": 3, "stars": 1 }
+  },
+  {
+    "id": "1-2-7",
+    "name": "Rocsolvre",
+    "category": "Sol",
+    "form": "Rochet",
+    "color": { "name": "Cuivre", "hex": "#b45309" },
+    "stats": { "hp": 4, "power": 6, "stars": 1 }
+  },
+  {
+    "id": "2-5-0",
+    "name": "Penplauge",
+    "category": "Plafond",
+    "form": "Pendu",
+    "color": { "name": "Rouge", "hex": "#ef4444" },
+    "stats": { "hp": 1, "power": 1, "stars": 0 }
+  },
+  {
+    "id": "0-5-9",
+    "name": "Fumvolyan",
+    "category": "Volant",
+    "form": "Fumee",
+    "color": { "name": "Cyan", "hex": "#06b6d4" },
+    "stats": { "hp": 10, "power": 6, "stars": 2 }
+  }
+]
---
+++ questions.js
@@ -0,0 +1,40 @@
+const questions = [
+  { id: 1, question: "What is the capital city of France?", answer: "Paris" },
+  {
+    id: 2,
+    question: "Which planet is known as the Red Planet?",
+    answer: "Mars",
+  },
+  {
+    id: 3,
+    question: "Who painted the Mona Lisa?",
+    answer: "Leonardo da Vinci",
+  },
+  {
+    id: 4,
+    question: "What is the largest ocean on Earth?",
+    answer: "Pacific Ocean",
+  },
+  { id: 5, question: "In which year did the Titanic sink?", answer: "1912" },
+  { id: 6, question: "What is the chemical symbol for gold?", answer: "Au" },
+  {
+    id: 7,
+    question: "Who developed the theory of relativity?",
+    answer: "Albert Einstein",
+  },
+  {
+    id: 8,
+    question: "Which country hosted the 2016 Summer Olympics?",
+    answer: "Brazil",
+  },
+  {
+    id: 9,
+    question: "What is the largest mammal in the world?",
+    answer: "Blue whale",
+  },
+  {
+    id: 10,
+    question: "How many continents are there on Earth?",
+    answer: "Seven",
+  },
+];
